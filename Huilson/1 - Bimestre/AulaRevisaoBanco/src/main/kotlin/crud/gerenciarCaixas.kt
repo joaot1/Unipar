bb06522cb2a65eea3b81023f90edbf3d4f95f244
joaot1 <joaotombinibortolotti@Gmail.com>
2025-09-02 20:20:31 -0300

    Código novo da aula huilsilis

--- Huilson/1 - Bimestre/AulaRevisaoBanco/src/main/kotlin/crud/gerenciarCaixas.kt
+++ Huilson/1 - Bimestre/AulaRevisaoBanco/src/main/kotlin/crud/gerenciarCaixas.kt
@@ -3,6 +3,33 @@ package org.example.crud
 import org.example.entidades.CaixaDAgua
 import org.example.enumeradoras.Material
 
+fun criarTabelaCaixa(){
+    // Precisamos puxar no main para funcionar
+    val conectar = EntidadeJDBC(
+        url = "jdbc:postgresql://localhost:5432/postgres",
+        usuario = "postgres",
+        senha = "5432",
+    )
+    //Coloque o nome da tabela o mesmo nome da entidade.
+    val sql = "CREATE TABLE IF NOT EXISTS CaixaDAgua" +
+            " (id serial NOT NULL PRIMARY KEY," +
+            " material varchar(255)," +
+            " capacidade float," +
+            " altura float," +
+            " largura float," +
+            " profundidade float" +
+            ")"
+//Cada coluna da tabela precisa ter o mesmo nome dos atributos da Entidade.
+
+    val banco = conectar.conectarComBanco()
+    val enviarParaBanco = banco!!.createStatement().execute(sql)
+    // !! Para dizer que esse cara nunca vai ser null e dai precisamos criar uma afirmação e executar nosso sql
+
+    println(enviarParaBanco) // Se retornar false ou 1, deu certo!
+
+    banco.close() //Encerra a conexão com o banco.
+}
+
 fun cadastrasCaixas(){
 
     println("Escolha o material do qual a caixa é composta:")
