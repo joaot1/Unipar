34823004b2c005b1190c326bdbf882a3f039cfdd
joaot1 <joaotombinibortolotti@Gmail.com>
2025-09-15 20:04:13 -0300

    Ta feito

--- Cleiton/4 Período - Front-End/1 Bimestre - Cleiton/CodigosAula/listatarefas/src/App.jsx
+++ Cleiton/4 Período - Front-End/1 Bimestre - Cleiton/CodigosAula/listatarefas/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       const novaTarefa = {
         id : Date.now(),
         nome : tarefa,
+        completada : false
       }
 
     setListaTarefas([...listaTarefas, novaTarefa])
@@ -23,6 +24,21 @@ function App() {
     alert("Adicionado com sucesso");
     setTarefa("");
   };
+
+  const excluirTarefa = (id) => {
+
+    setListaTarefas(listaTarefas.filter( (task) => task.id != id))
+
+  }
+
+  const alterarSituacao = (id) =>{
+    
+    setListaTarefas(
+      listaTarefas.map( (task) => (
+        task.id == id ? {...task, completada:!task.completada} : task
+      ))
+    )
+  }
 
   return (
     <div className="todo-container">
@@ -40,11 +56,15 @@ function App() {
 
       <ul>
         {listaTarefas.map((task) => (
-          <li key={task.id}>
+          <li key={task.id}
+          style={{
+            textDecoration: task.completada ? 'line-through' : 'none',
+            color: task.completada ? 'gray' : 'black'
+          }}>
             {task.nome}
             <div>
-              <button className="complete-btn">✔</button>
-              <button className="delete-btn">❌</button>
+              <button onClick={()=> alterarSituacao(task.id)} className="complete-btn">✔</button>
+              <button onClick={() => excluirTarefa(task.id)}  className="delete-btn">❌</button>
             </div>
           </li>
         ))}
@@ -60,4 +80,6 @@ Esse evento eu posso capturar ele e utilizar.
 Na polarização {}. Aqui pode ser mapeado e utilizado minha lista. 
 Sempre que quiser repetir algumas coisa sempre utilizamos um map.
 Date.now(), -> esse cara vai criar um hash com data, hora o local atual.
+o Map faz a idea de percorrer.
+
 */
