82aabe8ed2e2ae4e2d024c917fe021dd1e23e77f
joaot1 <joaotombinibortolotti@Gmail.com>
2025-09-16 19:53:15 -0300

    Codigo atividade 2

--- Huilson/4 - Período - BackEnd/1 - Bimestre/AtividadeHuilson/src/main/kotlin/Main.kt
+++ Huilson/4 - Período - BackEnd/1 - Bimestre/AtividadeHuilson/src/main/kotlin/Main.kt
@@ -1,13 +1,24 @@
-package org.example
+/**
+ * Nomes: João Antonio Tombini Bortolotti | RA: 60001145
+ * Ygor Rafael De Moura Artuzo | RA: 60001113
+ * Gabriel Henrique Mombelli | RA: 60002796
+ * João Schmitz | RA: 60003036
+ */
+
+import java.math.BigDecimal
 
 fun main() {
+    // Simulando dados vindos do banco (VARCHAR)
+    val idBanco = 1
+    val nomeBanco = "Cadeira"
+    val precoBanco = "199.90" // VARCHAR no banco
 
+    // Conversão de String (VARCHAR) para BigDecimal
+    val precoConvertido = BigDecimal(precoBanco)
+
+    // Criando objeto Produto com o valor convertido
+    val produto = Produto(idBanco, nomeBanco, precoConvertido)
+
+    // Exibindo o objeto no console
+    println(produto)
 }
-
-
-/**
- * Nomes: João Antonio Tombini Bortolotti | RA:60001145
- * Ygor rafael De Moura artuzo | RA : 60001113
- * Gabriel Henrique Mombelli | RA : 60002796
- * Joao Schmitz | RA : 60003036
- * **/
---
+++ Huilson/4 - Período - BackEnd/1 - Bimestre/AtividadeHuilson/src/main/kotlin/Produto.kt
@@ -0,0 +1,9 @@
+
+import java.math.BigDecimal
+
+// Classe representando a tabela Produto
+data class Produto(
+    val id: Int,
+    val nome: String,
+    val preco: BigDecimal
+)
